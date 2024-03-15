509b40981f97250341791ee25882b99f3059883c
João Victor Barbosa da Silva <joaobs98@hotmail.com>
2024-03-15 15:54:56 -0300

    Feat container

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md="auto">
+          <h1>Jogo da Mega Sena</h1>
+          <Button variant="primary">Gerar Jogo</Button>
+          <div className="mt-3">
+            <h2>Números Sorteados:</h2>
+            <ul>
+              {}
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default App;
